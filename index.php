d4ecd8d63c1637c22baf66aae701ab6c1b9c3957
김태호 <xogh504@gmail.com>
2019-03-15 15:48:08 +0900

    3/15 company

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
     <script src="assets/js/commonActions.js"></script>
 
 </head>
@@ -17,7 +17,24 @@
     <div id="pageContainer">
 
         <div id="mastHeadContainer">
-            <button class="navShowHide">Click me!</button>
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+            <!--로그 클릭하면 기본 화면으로 이동해야하니 a태그사용-->
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/VideoTubeLogo.png" title="logo" alt="site logo">
+            </a>
+
+            <div class = "searchBarContainer">
+                <!--form은 입력을위한 HTMl양식만드는데 사용, action은 url연결, method는 방식-->
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png">
+                    </button>
+                </form>
+
+            </div>
         </div>
 
         <div id="sideNavContainer" style="display:none;">
@@ -36,3 +53,4 @@
 
 </body>
 </html>
+
